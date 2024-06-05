96a0e647581e84a4596c1fd0a98b727bd1206e9e
juknow <munjunho77@naver.com>
2024-06-06 03:58:58 +0900

    [SETTING] 초기 세팅

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header"></header>
+      <div className="header-frame1">
+        <img />
+        <input />
+      </div>
+      <div className="header-frame2"></div>
+      <main className="main"></main>
+      <footer className="footer">
+        <div className="footer-text">
+          Made with ♥ by KUCC <br />
+          <br />
+          서울특별시 성북구 고려대로 105 고려대학교 학생회관 314호
+          <br />
+          anamkucc314@gmail.com
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
